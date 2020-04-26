908303050f1ad259f4d5dbbdcbc6c76c80c8d471
Oğuz ÖZGÜL <oguzhannet@gmail.com>
2020-04-27 01:42:16 +0300

    no message

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 import MapKit
-
-class ViewController: UIViewController, MKMapViewDelegate {
+import CoreLocation
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         mapView.delegate = self
+        locationManager.delegate = self // Lokalizayonu Göster
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // Ne Kadar Tahmin edecek Best En İyisi
+        locationManager.requestWhenInUseAuthorization() // Kullanıcı İzni WheninUse program çalıştığında
+        locationManager.startUpdatingLocation() // kullanıcıyı yeri almaya başlıyor
+        
+        
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.latitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    }
 
 }
 
